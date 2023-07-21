b625168af5f9c0a5f635a6124def867ead358e22
PHadaia <philipp.hadaia@outlook.de>
2023-07-22 00:44:27 +0200

    Extract the used Constants into a TestConstants class

---
+++ starter_code/src/test/java/com/example/demo/TestConstants.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class TestConstants {
+    public static final long USER_ID = 0L;
+    public static final long ITEM_ID = 0L;
+    public static final long ORDER_ID = 0L;
+    public static final String USERNAME = "testUsername";
+    public static final String USER_PASSWORD = "testPassword";
+    public static final String ITEM_NAME = "testItemName";
+
+}
